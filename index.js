da647c07bad7acc125274e0c567d615f125c3393
Aakil Fernandes <aakilfernandes@gmail.com>
2018-01-18 16:27:20 -0500

    upgrade for amorph@4.0

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-const Nobject = require('nobject')
-const converters = new Nobject
-const arrayEquals = require('array-equal')
+const AmorphConverter = require('amorph-converter')
 
-converters.set(['hex', 'array'], (hex) => {
-  const array = []
-  for (let i = 0; i < hex.length; i += 2) {
-    const hexPart = hex.substr(i,2)
-    const int = parseInt(hexPart, 16)
-    array.push(int)
-  }
-  return array
-})
-
-converters.set(['array', 'hex'], (array) => {
-  return array.map((int) => {
-
+const unprefixed = new AmorphConverter((uint8Array) => {
+  return Array.from(uint8Array).map((int) => {
     const hexPart = int.toString(16)
-
     if (int === 0) {
       return '00'
     } else if (int < 16) {
@@ -24,23 +10,21 @@ converters.set(['array', 'hex'], (array) => {
     } else {
       return hexPart
     }
-
   }).join('')
-})
-
-converters.set(['hex', 'hex.prefixed'], (hex) => {
-  return '0x'+hex
-})
-
-converters.set(['hex.prefixed', 'hex'], (prefixedHex) => {
-  return prefixedHex.substr(2)
-})
-
-
-module.exports = {
-  pluginVersion: 1,
-  converters: converters,
-  equivalenceTests: {
-    array: arrayEquals
+}, (hex) => {
+  const array = []
+  for (let i = 0; i < hex.length; i += 2) {
+    const hexPart = hex.substr(i,2)
+    const int = parseInt(hexPart, 16)
+    array.push(int)
   }
-}
+  return new Uint8Array(array)
+})
+
+const prefixed = new AmorphConverter((uint8Array) => {
+  return '0x' + unprefixed.to(uint8Array)
+}, (hex) => {
+  return unprefixed.from(hex.slice(2))
+})
+
+module.exports = { prefixed, unprefixed }
---
+++ test.js
@@ -0,0 +1,26 @@
+const converters = require('./')
+const chai = require('chai')
+
+chai.should()
+
+describe('converters', () => {
+
+  describe('unprefixed', () => {
+    it('should .to', () => {
+      converters.unprefixed.to(new Uint8Array([1, 2, 3, 4])).should.equal('01020304')
+    })
+    it('should .from', () => {
+      converters.unprefixed.from('01020304').should.deep.equal(new Uint8Array([1, 2, 3, 4]))
+    })
+  })
+
+  describe('prefixed', () => {
+    it('should .to', () => {
+      converters.prefixed.to(new Uint8Array([1, 2, 3, 4])).should.equal('0x01020304')
+    })
+    it('should .from', () => {
+      converters.prefixed.from('0x01020304').should.deep.equal(new Uint8Array([1, 2, 3, 4]))
+    })
+  })
+
+})
